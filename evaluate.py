fb9464a6aa0319bcc8f47c70db082a922302bf66
Eli Browne <browne@scdt.stanford.edu>
2024-07-01 14:06:15 -0700

    Updated for validation (not test), improved evaluation efficiency

--- evaluate.py
+++ evaluate.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     # Gather queries for evaluating retrieval. These can be passed to a model as an ordered list.
 
-    qa_data = datasets.load_dataset("retrieval-bar/mbe", name="qa", split=datasets.Split.TEST, trust_remote_code=True)
+    qa_data = datasets.load_dataset("retrieval-bar/mbe", name="qa", split=datasets.Split.VALIDATION, trust_remote_code=True)
     # Remove items with no PID (not relevant to evaluating retrieval)
     qa_data = qa_data.filter(lambda entry : entry["gold_idx"] != "nan") # blanks are listed as "nan"
     # Concatenate prompt (when applicable) and question to get a list of queries
@@ -25,24 +25,30 @@ if __name__ == "__main__":
     # We can do this iteratively. Metrics in the "Legal Retrievers May..." paper for evaluating
     # BarQA included Recall@1, Recall@10, MRR@10, Recall@100, and Recall@1000.
 
-    k = 1 
+    k = 1000 
     save_file = "evaluation_basecolbert.txt" # change this when testing different models
-    while k <= 1000:
-        retrieval_results = model.search(queries, k = k) # efficiency?
-        retrieved_gold_passages = 0 # count of queries that retrieved the gold passage
-        mrr_total = 0 # sum of MRR values (for mean MRR calculation)
+    retrieval_results = model.search(queries, k = k) 
+    retrieved_1, retrieved_10, retrieved_100, retrieved_1000 = 0, 0, 0, 0 # count of queries that retrieved the gold passage @ k = 1, 10...
+    mrr_total_10 = 0 # sum of MRR values @ k = 10 (for mean MRR calculation)
 
-        for i, results in enumerate(retrieval_results):
-            for j, result in enumerate(results):
-                if result["document_id"] == gold_passage_ids[i]: # gold_idx[i] and query[i] (-> result[i]) should be the same example
-                    retrieved_gold_passages += 1
-                    mrr_total += 1 / (j + 1) # 1 / (0 + 1) for first result and so on
-                    break # break as soon as gold passage is found for efficiency
-        
-        with open(save_file, 'a') as f:
-            f.write("Results at K = " + str(k) + "\n")
-            f.write(" - Recall (average, %): " + str(100 * retrieved_gold_passages / len(retrieval_results)) + "\n")
-            f.write(" - MRR (average, %): " + str(100 * mrr_total / len(retrieval_results)) + "\n")
-            f.write("\n")
-        
-        k *= 10
+    for i, results in enumerate(retrieval_results):
+        for j, result in enumerate(results):
+            if result["document_id"] == gold_passage_ids[i]: # gold_idx[i] and query[i] (-> result[i]) should be the same example
+                if j + 1 <= 1000: 
+                    retrieved_1000 += 1
+                    if j + 1 <= 100: 
+                        retrieved_100 += 1
+                        if j + 1 <= 10:
+                            retrieved_10 += 1
+                            mrr_total_10 += 1 / (j + 1)
+                            if j + 1 == 1: retrieved_1 += 1
+                break # break as soon as gold passage is found for efficiency
+    
+    with open(save_file, 'a') as f:
+        f.write("Results at K = " + str(k) + "\n")
+        f.write(" - Recall (%) @ K = 1: " + str(100 * retrieved_1 / len(retrieval_results)) + "\n")
+        f.write(" - Recall (%) @ K = 10: " + str(100 * retrieved_10 / len(retrieval_results)) + "\n")
+        f.write(" - MRR (average, %): " + str(100 * mrr_total_10 / len(retrieval_results)) + "\n")
+        f.write(" - Recall (%) @ K = 100: " + str(100 * retrieved_100 / len(retrieval_results)) + "\n")
+        f.write(" - Recall (%) @ K = 1000: " + str(100 * retrieved_1000 / len(retrieval_results)) + "\n")
+        f.write("\n")
--- generate_index.py
+++ generate_index.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     model_name = "colbert-ir/colbertv2.0" # substitute with model being employed!
     model = RAGPretrainedModel.from_pretrained(model_name)
 
-    passage_data = datasets.load_dataset("retrieval-bar/mbe", name="passages", split=datasets.Split.TEST, trust_remote_code=True)
+    passage_data = datasets.load_dataset("retrieval-bar/mbe", name="passages", split=datasets.Split.VALIDATION, trust_remote_code=True)
     passages, passage_ids = passage_data["text"], passage_data["idx"]
     # The index is stored locally: it's named based on the model it used; it uses passage IDs as document IDs. 
     index_path = model.index(index_name="index_colbertv2.0base_gpu", collection=passages, document_ids=passage_ids, bsize=8, max_document_length=512) # document length maximized for BERT
