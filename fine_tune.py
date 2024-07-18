697b9e9dafb6ca341d562d2e74ca0e83ca01efe6
Eli Browne <browne@jagupard37.stanford.edu>
2024-07-18 14:45:37 -0700

    Baseline results for all on same data, no WandB analysis

--- fine_tune.py
+++ fine_tune.py
@@ -2,6 +2,7 @@
 
 from ragatouille import RAGTrainer
 import datasets
+import wandb
 
 if __name__ == "__main__":
     # Collect pairs of (query, gold passage) tuples for training data
@@ -20,7 +21,7 @@ if __name__ == "__main__":
     passages = passage_data["text"]
 
     # Set up RAGTrainer with model that we're looking to fine-tune
-    trainer = RAGTrainer(model_name = "legalBERT", pretrained_model_name = "casehold/custom-legalbert")
-    trainer.prepare_training_data(raw_data=data, all_documents=passages, data_out_path="./data/") # this should automatically mine hard negatives
+    trainer = RAGTrainer(model_name = "ftLegalBERT", pretrained_model_name = "casehold/custom-legalbert")
+    # trainer.prepare_training_data(raw_data=data, all_documents=passages, data_out_path="./data/") # this should automatically mine hard negatives
 
     trainer.train(batch_size = 32) # default batch size = 32
